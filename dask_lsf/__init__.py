6a2cc9d752c667298f738b41f47388011a54e694
akarshan <akarshan@qti.qualcomm.com>
2021-09-18 16:08:11 +0530

    changed version to 1.0.0 in dask_lsf/__init__.py

--- dask_lsf/__init__.py
+++ dask_lsf/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Akarshan Arora"""
 __email__ = 'akarshanarora@gmail.com'
-__version__ = '0.1.0'
+__version__ = '1.0.0'
 
 from dask_lsf.dask_lsf import setuplsfcluster
 from dask_lsf.dask_lsf import setuplsfclient

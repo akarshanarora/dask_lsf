a00d538f43231e9caced8ed2c628022660cb16a3
akarshan <akarshan@qti.qualcomm.com>
2021-09-18 13:16:16 +0530

    added direct access to closesystem, waitforworkers, launchlsfjobs

--- dask_lsf/__init__.py
+++ dask_lsf/__init__.py
@@ -8,3 +8,6 @@ from dask_lsf.dask_lsf import setuplsfcluster
 from dask_lsf.dask_lsf import setuplsfclient
 from dask_lsf.dask_lsf import get_dashboard_port
 from dask_lsf.dask_lsf import setupsystem
+from dask_lsf.dask_lsf import closesystem
+from dask_lsf.dask_lsf import launchlsfjobs
+from dask_lsf.dask_lsf import waitforworkers

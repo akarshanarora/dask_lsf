902960e9d6b98d74405ca813de9a4eaf032a5a82
Akarshan Arora <akarshan@qti.qualcomm.com>
2021-09-18 09:55:41 +0530

    first commit

---
+++ README.rst
@@ -0,0 +1,37 @@
+========
+dask_lsf
+========
+
+
+.. image:: https://img.shields.io/pypi/v/dask_lsf.svg
+        :target: https://pypi.python.org/pypi/dask_lsf
+
+.. image:: https://img.shields.io/travis/akarshanarora/dask_lsf.svg
+        :target: https://travis-ci.com/akarshanarora/dask_lsf
+
+.. image:: https://readthedocs.org/projects/dask-lsf/badge/?version=latest
+        :target: https://dask-lsf.readthedocs.io/en/latest/?version=latest
+        :alt: Documentation Status
+
+
+
+
+Run code on lsf farm using dask
+
+
+* Free software: Apache Software License 2.0
+* Documentation: https://dask-lsf.readthedocs.io.
+
+
+Features
+--------
+
+* TODO
+
+Credits
+-------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+requirements = [ ]
+
+test_requirements = [ ]
+
+setup(
+    author="Akarshan Arora",
+    author_email='akarshanarora@gmail.com',
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Run code on lsf farm using dask",
+    install_requires=requirements,
+    license="Apache Software License 2.0",
+    long_description=readme,
+    include_package_data=True,
+    keywords='dask_lsf',
+    name='dask_lsf',
+    packages=find_packages(include=['dask_lsf', 'dask_lsf.*']),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/akarshanarora/dask_lsf',
+    version='0.0.1',
+    zip_safe=False,
+)
